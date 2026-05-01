1e9bb798cdb65be4b465b6ccf994ed18d2a09111
Biswajit <pal.bisw95@gmail.com>
2026-05-01 08:57:43 +0530

    Test encuplation

---
+++ encapsulation/TestEncapsulation.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class TestBean {
+    private int data; // Private variable
+
+    public int getData() { // Public getter method
+        return data;
+    }
+    public void setData(int data) { // Public setter method
+        this.data = data;
+    }
+    
+}

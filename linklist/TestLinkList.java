dcda550090ed01077f743680b119d8a8eb16e9c4
Biswajit <pal.bisw95@gmail.com>
2026-07-07 07:01:24 +0530

    circular link list

--- linklist/TestLinkList.java
+++ linklist/TestLinkList.java
@@ -13,29 +13,53 @@ class Node {
 }
 
 public class TestLinkList {
+
+    void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+    void addNode(Node head, int data) {
+        Node newNode = new Node(data);
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
     public static void main(String[] args) {
-        // Node n1 = new Node(10);
-        // Node n2 = new Node(20);
-        // n1.next = n2;
-        // System.out.println(n1.data); // 10
-        // System.out.println(n1.next.data); // 10
+        
+        TestLinkList ob = new TestLinkList();
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
 
-        LinkedList<Integer> list = new LinkedList<>();
+        ob.printList(head);
 
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        System.out.println(list);
-        System.out.println(list.get(2)); // 30
-        System.out.println(list.getFirst()); // 10 
-        System.out.println(list.getLast()); // 40
-        System.out.println(list.size()); // 4
-        list.addFirst(5);
-        list.addLast(50);
-        System.out.println(list); // [5, 10, 20, 30, 40, 50]
-        list.removeIf(x -> x%2==0);
-        System.out.println(list); // [5]
+        ob.addNode(head, 50);
+        ob.printList(head);
+
+        // LinkedList<Integer> list = new LinkedList<>();
+
+        // list.add(10);
+        // list.add(20);
+        // list.add(30);
+        // list.add(40);
+        // System.out.println(list);
+        // System.out.println(list.get(2)); // 30
+        // System.out.println(list.getFirst()); // 10 
+        // System.out.println(list.getLast()); // 40
+        // System.out.println(list.size()); // 4
+        // list.addFirst(5);
+        // list.addLast(50);
+        // System.out.println(list); // [5, 10, 20, 30, 40, 50]
+        // list.removeIf(x -> x%2==0);
+        // System.out.println(list); // [5]
+
+        
     
     }
     
